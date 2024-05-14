00898b758f6ba03fc676e0fe7fd524f05adb8053
Nikhil Boghani <131741338+NikhilBoghani1@users.noreply.github.com>
2024-05-14 17:04:52 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,1 @@
+import 'package:car_shop_ui/view/home_view.dart';import 'package:flutter/material.dart';void main() => runApp(MyApp());class MyApp extends StatelessWidget {  @override  Widget build(BuildContext context) {    return MaterialApp(      debugShowCheckedModeBanner: false,      title: 'Material App',      home: HomeView(),    );  }}
